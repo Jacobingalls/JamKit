1461331fac1f1da4ea7ace80029b8b5f751df5cf
James Kelly <dylan_j_kelly@icloud.con>
2023-10-08 17:32:39 -0700

    Add pubsubsender editor

---
+++ Runtime/PubSub/Editor/PubSubSenderEditor.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace info.jacobingalls.jamkit
+{
+    [CustomEditor(typeof(PubSubSender))]
+    public class PubSubSenderEditor : Editor
+    {
+        private bool _debug;
+        private string _eventName;
+
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+
+            _debug = EditorGUILayout.Toggle("Debug", _debug);
+            if (!_debug) { return; }
+
+            EditorGUILayout.TextField("Event Name", _eventName);
+
+            EditorGUI.BeginDisabledGroup(Application.isEditor);
+            var pubSubSender = target as PubSubSender;
+            if (GUILayout.Button("Send"))
+            {
+                pubSubSender.Publish(_eventName);
+            }
+            EditorGUI.EndDisabledGroup();
+        }
+    }
+}
