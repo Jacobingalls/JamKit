dc9501426d43769503b47b09259e9690f5133f19
James Kelly <dylan_j_kelly@icloud.con>
2025-04-05 18:56:35 -0700

    Update AudioManager

--- Runtime/General/Audio/AudioManager.cs
+++ Runtime/General/Audio/AudioManager.cs
@@ -55,13 +55,13 @@ public class AudioManager : MonoBehaviour
         return Play(id);
     }
 
-    public static AudioSource Play2D(this AudioManager am, string id, bool loop = false, float pitchMin = 1.0f, float pitchMax = 1.0f, float volumeMin = 1.0f, float volumeMax = 1.0f, bool isMusic = false, Vector3? position = null, float minDistance = 0.01f, float maxDistance = 20.0f)
+    public AudioSource Play2D(string id, bool loop = false, float pitchMin = 1.0f, float pitchMax = 1.0f, float volumeMin = 1.0f, float volumeMax = 1.0f, bool isMusic = false, Vector3? position = null, float minDistance = 0.01f, float maxDistance = 20.0f)
     {
         if (position != null)
         {
             position = new(Camera.main.transform.position.x, position.Value.y, Camera.main.transform.position.z);
         }
-        return am.Play(id, loop, pitchMin, pitchMax, volumeMin, volumeMax, isMusic, position, minDistance, maxDistance);
+        return Play(id, loop, pitchMin, pitchMax, volumeMin, volumeMax, isMusic, position, minDistance, maxDistance);
     }
 
     public AudioSource Play(string id, bool loop = false, float pitchMin = 1.0f, float pitchMax = 1.0f, float volumeMin = 1.0f, float volumeMax = 1.0f, bool isMusic = false, Vector3? position = null, float minDistance = 1, float maxDistance = 1000)
