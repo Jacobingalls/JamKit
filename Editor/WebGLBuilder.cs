54cfe5551880e2a0bf49c4166d42d1b17939839d
Jacob Ingalls <jacobingalls@yahoo.com>
2025-04-16 23:08:34 -0700

    Wrap build helper in if to fix build issue

--- Editor/WebGLBuilder.cs
+++ Editor/WebGLBuilder.cs
@@ -1,3 +1,5 @@
+#if UNITY_EDITOR
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -25,3 +27,5 @@ public class WebGLBuilder
       BuildPipeline.BuildPlayer(scenes, buildPath, BuildTarget.WebGL, BuildOptions.None); 
     }  
 }
+
+#endif
